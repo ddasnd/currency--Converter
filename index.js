c1a8259d375536d4ecc77ca60785744b4cb8b894
ddasnd <139565074+ddasnd@users.noreply.github.com>
2023-07-22 23:26:26 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const fromCur = document.querySelector(".from select");
+const toCur = document.querySelector(".to select");
+const getBtn = document.querySelector("form button");
+const exIcon = document.querySelector("form.reverse");
+const amount = document.querySelector("from input");
+const exRateTxt = document.querySelector("from .result");
+
+// Event listener for currency dropdowns (select)
+
+[fromCur, toCur].forEach((select, i) => {
+    for (let curCode in Country_List) {
+        const selected = (i === 0 && curCode === "USD") || (i === 1 && curCode === "PKR") ? "selected" : "";
+        select.insertAdjacementHTML("beforeend", `<option value="${curCode}" ${selected}>${curCode}</option`);
+    }
+    select.addEventListener("change", () => {
+        const code = select.value;
+        const imgTag = select.parentElement.querySelector("img");
+        imgTag.src = `https://flagcdn.com/48x36/${Country_List[code].toLowerCase()}.png`;
+    })
+});
+
+// Function to get exchange rate from api
+
+async function getExchangeRate() {
+    const amountVal = amount.value || 1;
+    exRateTxt.innerText = "Getting exchange rate...";
+    try{
+        const response = await fetch(`https://v6.exchangerate-api.com/v6/[YOUR_KEY]]/latest/${fromCur.value}`);
+        const result = await response.json();
+        const exchangerate = result.conversion_rates[toCur.value];
+        const totalExRate = (amountVal * exchangerate). toFixed(2);
+        exRateTxt.innerText = `${amountVal} ${fromCur.value} = ${totalExRate} ${toCur.value}`;
+    } catch (error) {
+        exRateTxt.innerText = "Something went wrong...";
+    }
+}
+
+// Event listeners for button and exchange icon click
+
+window.addEventListener("load", getExchangeRate);
+getBtn,addEventListener("click", (e) => {
+    e.preventDefault();
+    getExchangeRate();
+})
+
+exIcon.addEventListener("click", () => {
+    [fromCur.value, toCur.value] = [toCur.value, fromCur.value]
+    [fromCur, toCur].forEach((select) => {
+        const code = select.value;
+        const imgTag = select.parentElement.querySelector("img");
+        imgTag.src = `https://flagcdn.com/48x36/${Country_List[code].toLowerCase()}.png`;
+    });
+    getExchangeRate();
+});
+
+
+    
+
+
+
